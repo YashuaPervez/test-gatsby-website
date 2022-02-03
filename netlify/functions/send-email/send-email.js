e82e47263df0452c21360e837e5b1fb3038ef47c
Yashua Pervez <yashuapervez@gmail.com>
2022-02-04 00:09:11 +0500

    Send email function updated

--- netlify/functions/send-email/send-email.js
+++ netlify/functions/send-email/send-email.js
@@ -1,36 +1,64 @@
 const nodemailer = require("nodemailer");
+const joi = require("joi");
 
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const sendEmailSchema = joi.object().keys({
+  name: joi.string().min(2).required(),
+  email: joi.string().email().required(),
+  phone: joi.number().required(),
+});
+
 const handler = async (event) => {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Only post request is allowed" }),
+    };
+  } else {
+    const { error } = sendEmailSchema.validate(JSON.parse(event.body));
+    if (error) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          success: false,
+          message: error?.details?.[0]?.message,
+        }),
+      };
+    }
+  }
   try {
+    const body = JSON.parse(event.body);
+    const output = `<ul>
+    <li>Name: ${body.name}</li>
+    <li>Email: ${body.email}</li>
+    <li>Phone: ${body.phone}</li>
+    </ul>`;
+
     const transporter = nodemailer.createTransport({
       host: "mail.yashuapervez.com",
       port: 465,
       secure: true,
       auth: {
-        user: "yashuapervez@yashuapervez.com",
-        pass: "#;Gfbn?i{56?]+-QJD",
-      },
-      tls: {
-        rejectUnauthorized: false,
+        user: process.env.EMAIL_USER_NAME,
+        pass: process.env.EMAIL_PASS,
       },
     });
     const mailOptions = {
-      from: '"Yashua Pervez" <yashuapervez@yashuapervez.com>',
+      from: `"Yashua Pervez" <${process.env.EMAIL_USER_NAME}>`,
       to: "yashuapervez@gmail.com",
-      subject: "Hello",
-      html: "<p>Hi, this works</p>",
+      subject: "Message from your website",
+      html: output,
     };
 
     const response = await transporter.sendMail(mailOptions);
-    console.log("response >>", response);
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello World` }),
+      body: JSON.stringify({
+        message: "Email sent successfully",
+        success: true,
+      }),
     };
   } catch (error) {
-    console.log("error >>", error);
     return { statusCode: 500, body: error.toString() };
   }
 };
